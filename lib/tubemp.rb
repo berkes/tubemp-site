84f3541a648d0a63c07bf9df144d6a298633b8c4
Bèr Kessels <ber@webschuur.com>
2013-04-09 11:43:23 +0200

    make file executable.

--- lib/tubemp.rb
+++ lib/tubemp.rb
@@ -4,26 +4,28 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'youtube')
 require File.join(File.expand_path(File.dirname(__FILE__)), 'thumbnail')
 include ERB::Util
 
-# when using from Rackup
-enable :inline_templates
+class Tubemp < Sinatra::Application
+  # when using from Rackup
+  enable :inline_templates
 
-get '/tags.?:format?' do
-  yt  = YouTube.new params[:v]
-  uri = URI(request.base_url)
+  get '/tags.?:format?' do
+    yt  = YouTube.new params[:v]
+    uri = URI(request.base_url)
 
-  if yt.valid?
-    case params[:format]
-    when "json"
-      content_type :json
-      yt.tags(uri).to_json
-    else
-      erb :tags, :locals => {:tags => yt.tags(uri), :title => yt.title}
+    if yt.valid?
+      case params[:format]
+      when "json"
+        content_type :json
+        yt.tags(uri).to_json
+      else
+        erb :tags, :locals => {:tags => yt.tags(uri), :title => yt.title}
+      end
+    else # not valid
+      not_found erb(:not_found, :locals => { :title => "Not Found", :id => params[:v]} )
     end
-  else # not valid
-    not_found erb(:not_found, :locals => { :title => "Not Found", :id => params[:v]} )
   end
-end
 
-get '/' do
-  erb :index, :locals => { :title => "YouTube embeds without third party trackers." }
+  get '/' do
+    erb :index, :locals => { :title => "YouTube embeds without third party trackers." }
+  end
 end
