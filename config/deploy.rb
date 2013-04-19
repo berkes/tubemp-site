e4d724b01f333b5471b0c71d97999a9570c4c1f0
Bèr Kessels <ber@webschuur.com>
2013-04-19 10:38:11 +0200

    deploy.rb from e835042

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,22 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
+
+set :application, "ET_tubemp"
+set :repository,  "git@github.com:berkes/tubemp.git"
+set :deploy_to, "/var/www/#{application}"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "li308-140.members.linode.com" # Your HTTP server, Apache/etc
+role :app, "li308-140.members.linode.com" # This may be the same as your `Web` server
+# Gnome-terminal or Ubuntu or whatever trows errors. No idea what 
+# this does, but it fixes the issue. :D
+default_run_options[:pty] = true
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# Agent forwarding: use my local priv ssh keys for access to github.
+ssh_options[:forward_agent] = true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :shared_children, shared_children + %w{public/thumbs} #Add uploads to be shared. 
 
 # If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
@@ -23,3 +26,5 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+
